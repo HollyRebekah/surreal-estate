925d0f1838ff0a13044cfef1e4552479630c6ff4
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2019-09-01 11:14:21 +0100

    Styled server error message

--- src/components/alert.jsx
+++ src/components/alert.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../styles/alert.css';
 
 const Alert = () => {
   return (
-    <div className="Alert">Error!</div>
+    <div className="Alert" message="Error!">There is a problem with the server. Please try again later</div>
   );
 };
 
